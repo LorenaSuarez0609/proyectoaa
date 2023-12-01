8446f917f40c8b9064aa46b597c731235e3de95a
Maria de los Angeles Duran <128532593+mariadelosangelesduran@users.noreply.github.com>
2023-12-01 17:05:42 -0300

    subo Welcome modificado

--- approyecto/src/components/Welcome.jsx
+++ approyecto/src/components/Welcome.jsx
@@ -1,9 +1,43 @@
+//import {useState} from 'react';
 import React from 'react';
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
+import { useTodo } from './hooks/useTodo';
 
 
-const Welcome = () => {
-    return(
-        <div> Aplicacion Lista de tareas</div>
-    )
+function Welcome()  {
+    const {
+        todos, 
+        todosCount,
+        pendingTodosCount,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleCompleteTodo,
+        handleUpdateTodo,
+    } = useTodo();
+      
+    return ( 
+        <>
+            <div clasName="card-to-do">
+                <h1>Lista de Tareas</h1>
+                <div className="counter-todos">
+                    <h3> N° Tareas: <span>{todosCount}</span></h3>
+                    <h3> Pendientes: <span>{pendingTodosCount}</span></h3>
+                </div>
+      
+                <div className="add-todo">
+                    <h3>Agregar Tarea</h3>
+                    <TodoAdd handleNewTodo = {handleNewTodo}/>
+                </div>
+        
+                <TodoList 
+                    todos={todos}
+                    handleUpdateTodo = {handleUpdateTodo}
+                    handleDeleteTodo = {handleDeleteTodo}
+                   handleCompleteTodo = {handleCompleteTodo}
+                />
+            </div>
+        </>
+    );
 }
 export default Welcome;
